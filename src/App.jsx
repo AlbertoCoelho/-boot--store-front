080a7703e33b81fef67bb03fd724e1cdf6851c81
Alberto <tejotaesi@gmail.com>
2022-05-10 19:19:25 -0300

    feat: Added layout on login page

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { BrowserRouter as Router, Routes, Route, Navigate  } from "react-router-dom";
+import { useContext } from "react";
+
+import Login from "./pages/Login";
+// import SignUp from "./pages/SignUp";
+// import MainPage from "./pages/MainPage";
+
+import GlobalStyle from "./styles/GlobalStyle";
+
+import { AuthProvider, AuthContext } from './contexts/auth';
+
+const App = () => {
+  const Private = ( {children} ) => {
+  const { authenticated } = useContext(AuthContext);
+
+  if(!authenticated){
+    return <Navigate to="/" />
+  }
+
+  return children;
+  }
+
+
+  return (
+    <Router>
+      <AuthProvider>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          {/* <Route path="/sign-up" element={<SignUp />} />
+          <Route path="cash-income" element={<Private> <MainPage /> </Private>} /> */}
+        </Routes>
+        <GlobalStyle />
+      </AuthProvider>
+    </Router>
+  );
+}
+
+export default App;
--- src/services/api.js
+++ src/services/api.js
@@ -3,3 +3,12 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: `${process.env.REACT_APP_API_BASE_URL}`
 });
+
+export const makeSignUp = async (formData) => {
+  await api.post("/sign-up",formData)
+}
+
+export const makeLogin = async (formData) => {
+  const token = await api.post("/",formData);
+  return token;
+}
