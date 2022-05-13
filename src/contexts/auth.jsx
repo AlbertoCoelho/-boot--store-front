00e95ce9ada52c3ff5d347f076de04e459dadec5
acfernanda <acfernanda98@gmail.com>
2022-05-13 10:49:05 -0300

    fix auth

--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -1,68 +1,63 @@
 import { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import { api,makeLogin } from "../services/api";
+import { api, makeLogin } from "../services/api";
 
 export const AuthContext = createContext();
 
-export const AuthProvider = ( {children} ) => {
-
+export const AuthProvider = ({ children }) => {
   const navigate = useNavigate();
   const [user, setUser] = useState(null);
-  const [loading,setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
-  useEffect( () => {
-    const recoveredUser = localStorage.getItem('user');
-    // const token = localStorage.getItem("token");
+  useEffect(() => {
+    const recoveredUser = localStorage.getItem("user");
 
-    if(recoveredUser){
-      setUser(JSON.parse(recoveredUser));
+    if (recoveredUser) {
+      setUser(recoveredUser);
       api.defaults.headers.Authorization = recoveredUser;
     }
 
     setLoading(false);
+  }, []);
 
-  }, [])
-
-  const login = async (email,password,isLoading,setIsLoading) => {
+  const login = async (email, password, isLoading, setIsLoading) => {
     const formData = {
-      email:email,
-      password:password
-    }
+      email: email,
+      password: password,
+    };
     try {
       const response = await makeLogin(formData);
       console.log(response);
 
       const loggedUser = response.data;
-      // const token = response.data.token;
 
-      localStorage.setItem("user", JSON.stringify(loggedUser));
-      // localStorage.setItem("token", token);
-
+      localStorage.setItem("user", loggedUser);
       api.defaults.headers.Authorization = `Bearer ${loggedUser}`;
 
       setUser(loggedUser);
-      navigate("/MainPage");
-    } catch{
+      navigate("/");
+    } catch {
       alert("User not registered, make your registration!");
       isLoading.placeholder = "Entrar";
       isLoading.disabled = false;
-      setIsLoading({...isLoading});
+      setIsLoading({ ...isLoading });
     }
-  }
+  };
 
   const logout = () => {
     localStorage.removeItem("user");
-    // localStorage.removeItem("token");      
     api.defaults.headers.Authorization = null;
 
     setUser(null);
     navigate("/");
-  }
+  };
 
   return (
-    <AuthContext.Provider value= { {authenticated: !!user,user,login,logout,loading } }>
+    <AuthContext.Provider
+      value={{ authenticated: !!user, user, login, logout, loading }}
+    >
       {children}
     </AuthContext.Provider>
   );
-}
+};
